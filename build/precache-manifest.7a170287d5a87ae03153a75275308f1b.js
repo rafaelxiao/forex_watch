3b4f68a775b78b881a6c1804beee40e1dc909d20
ChickCake <owl@ChickCakes-MacBook-Air.local>
2023-01-12 10:33:37 +0800

    layout more

---
+++ build/precache-manifest.7a170287d5a87ae03153a75275308f1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c7637933563dd4b16a",
+    "url": "/forex_watch/static/css/main.e4e867d9.chunk.css"
+  },
+  {
+    "revision": "a0c7637933563dd4b16a",
+    "url": "/forex_watch/static/js/main.a0c76379.chunk.js"
+  },
+  {
+    "revision": "e57f9e2dea2bd8eb7e47",
+    "url": "/forex_watch/static/js/1.e57f9e2d.chunk.js"
+  },
+  {
+    "revision": "fd1f8dd5bfc7e1e67f6c",
+    "url": "/forex_watch/static/js/runtime~main.fd1f8dd5.js"
+  },
+  {
+    "revision": "592b18a80d1fd131692effda0c047d60",
+    "url": "/forex_watch/index.html"
+  }
+];
